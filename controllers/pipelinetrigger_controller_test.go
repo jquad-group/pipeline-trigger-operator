1bc51cb6fc01b74defbc32197de3acc55c6a739d
Tihomir Yordanov <ty@jquad.de>
2022-10-26 17:34:27 +0200

    added ginkgo tests

--- controllers/pipelinetrigger_controller_test.go
+++ controllers/pipelinetrigger_controller_test.go
@@ -1,6 +1,5 @@
 /*
 Copyright 2021.
-
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
@@ -18,98 +17,125 @@ package controllers
 
 import (
 	"context"
+	"time"
 
+	sourcev1 "github.com/fluxcd/source-controller/api/v1beta1"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
-	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
 	pipelinev1alpha1 "github.com/jquad-group/pipeline-trigger-operator/api/v1alpha1"
 )
 
-var _ = Describe("PipelineTriggerController", func() {
-	var (
-		name      string
-		namespace string
-		request   reconcile.Request
+var _ = Describe("PipelineTrigger controller", func() {
+
+	const (
+		resourceName = "foo-1"
+
+		namespace = "default"
+
+		timeout  = time.Second * 10
+		duration = time.Second * 10
+		interval = time.Millisecond * 250
 	)
-	BeforeEach(func() {
-		name = "test-resource"
-		namespace = "test-namespace"
-		request = reconcile.Request{
-			NamespacedName: types.NamespacedName{
-				Name:      name,
-				Namespace: namespace,
-			},
-		}
-	})
-	Describe("PipelineTrigger CRD", func() {
-		var (
-			instance *pipelinev1alpha1.PipelineTrigger
-		)
-		Context("with one base S3V1 resource", func() {
-			BeforeEach(func() {
-				// Create a new resource using k8sClient.Create()
-				// I'm just going to assume you've done this in
-				// a method called createInstanceInCluster()
-				instance = createInstanceInCluster(name, namespace, instance)
-			})
-			AfterEach(func() {
-				// Remember to clean up the cluster after each test
-				deleteInstanceInCluster(instance)
-			})
-			It("should have created the correct pipeline trigger", func() {
-				// Some method where you've implemented Gomega Expect()'s
-				//assertResourceUpdated(instance)
-				Expect(instance).To(Equal(name))
-			})
-			// This polls the interior function for a result
-			// and checks it against a condition until either
-			// it is true, or the timeout has been reached
-			Eventually(func() error {
-				defer GinkgoRecover()
-				err := k8sClient.Get(context.TODO(), request.NamespacedName, instance)
-				return err
-			}, "10s", "1s").ShouldNot(HaveOccurred())
+
+	Context("When setting up the test environment", func() {
+
+		It("Should fail to create a PipelineRun custom resources", func() {
+			By("Creating a GitRepository custom resource")
+
+			ctx := context.Background()
+			gitRepository := sourcev1.GitRepository{
+				ObjectMeta: v1.ObjectMeta{
+					Name:      resourceName,
+					Namespace: namespace,
+				},
+				Spec: sourcev1.GitRepositorySpec{
+					URL:      "http://github.com/org/repo.git",
+					Interval: v1.Duration{},
+				},
+				//Status: gitRepoStatus,
+			}
+			Expect(k8sClient.Create(ctx, &gitRepository)).Should(Succeed())
+
+			By("Creating a PipelineTrigger custom resource, referencing not existing Pipeline custom resource")
+			param1 := pipelinev1alpha1.InputParam{
+				Name:  "test",
+				Value: "test",
+			}
+			var params []pipelinev1alpha1.InputParam
+			params = append(params, param1)
+
+			source := pipelinev1alpha1.Source{
+				Kind: "GitRepository",
+				Name: resourceName,
+			}
+
+			pipelineTrigger1 := pipelinev1alpha1.PipelineTrigger{
+				ObjectMeta: v1.ObjectMeta{
+					Name:      resourceName,
+					Namespace: namespace,
+				},
+				Spec: pipelinev1alpha1.PipelineTriggerSpec{
+					Source: source,
+					Pipeline: pipelinev1alpha1.Pipeline{
+						Name:              resourceName,
+						SericeAccountName: "test",
+						InputParams:       params,
+						Workspace: pipelinev1alpha1.Workspace{
+							Name:       "test",
+							Size:       "5Gi",
+							AccessMode: "test",
+						},
+					},
+				},
+			}
+			Expect(k8sClient.Create(ctx, &pipelineTrigger1)).Should(Succeed())
+
+			By("Updating the GitRepository status")
+
+			gitRepoCondition := v1.Condition{
+				Type:               "Ready",
+				Status:             v1.ConditionTrue,
+				Reason:             v1.StatusSuccess,
+				Message:            "Success",
+				LastTransitionTime: v1.Now(),
+			}
+			var gitRepoConditions []v1.Condition
+			gitRepoConditions = append(gitRepoConditions, gitRepoCondition)
+			gitRepoStatus := sourcev1.GitRepositoryStatus{
+				Artifact: &sourcev1.Artifact{
+					Checksum:       "cb0053b034ac7e74e2278b94b69db15871e9b3b40124adde8c585c1bdda48b25",
+					Path:           "gitrepository/flux-system/flux-system/dc0fd09d0915f47cbda5f235a8a9c30b2d8baa69.tar.gz",
+					URL:            "http://source-controller.flux-system.svc.cluster.local./gitrepository/flux-system/flux-system/dc0fd09d0915f47cbda5f235a8a9c30b2d8baa69.tar.gz",
+					Revision:       "main/dc0fd09d0915f47cbda5f235a8a9c30b2d8baa69",
+					LastUpdateTime: v1.Now(),
+				},
+				Conditions: gitRepoConditions,
+			}
+			myGitRepo := &sourcev1.GitRepository{}
+			k8sClient.Get(ctx, types.NamespacedName{Name: resourceName, Namespace: namespace}, myGitRepo)
+			myGitRepo.Status = gitRepoStatus
+			Expect(k8sClient.Status().Update(ctx, myGitRepo)).Should(Succeed())
+
+			By("By checking the status of the PipelineTrigger CR")
+			myPipelineTrigger1 := &pipelinev1alpha1.PipelineTrigger{}
+
+			Eventually(func() (string, error) {
+				err := k8sClient.Get(ctx, types.NamespacedName{Name: resourceName, Namespace: namespace}, myPipelineTrigger1)
+				if err != nil {
+					return "", err
+				}
+
+				if len(myPipelineTrigger1.Status.GitRepository.Conditions) == 0 {
+					return "", nil
+				}
+
+				return myPipelineTrigger1.Status.GitRepository.Conditions[0].Reason, nil
+			}, timeout, interval).Should(ContainSubstring("Failed"), "Should have %s in the status", "Failed")
+
 		})
 	})
+
 })
-
-func deleteInstanceInCluster(instance *pipelinev1alpha1.PipelineTrigger) {
-	k8sClient.Delete(ctx, instance)
-}
-
-func createInstanceInCluster(name string, namespace string, instance *pipelinev1alpha1.PipelineTrigger) *pipelinev1alpha1.PipelineTrigger {
-	pipelineTrigger := &pipelinev1alpha1.PipelineTrigger{
-		TypeMeta: v1.TypeMeta{
-			APIVersion: "pipeline.jquad.rocks/v1alpha1",
-			Kind:       "PipelineTrigger",
-		},
-		ObjectMeta: v1.ObjectMeta{
-			Name:      name,
-			Namespace: namespace,
-		},
-		Spec: pipelinev1alpha1.PipelineTriggerSpec{
-			Source: pipelinev1alpha1.Source{
-				Kind: "ImagePolicy",
-				Name: "app1",
-			},
-			Pipeline: pipelinev1alpha1.Pipeline{
-				Name:              "build-and-push-base-image",
-				SericeAccountName: "default",
-				Workspace: pipelinev1alpha1.Workspace{
-					Name:       "workspace",
-					Size:       "1Gi",
-					AccessMode: "ReadWriteOnce",
-				},
-				InputParams: []pipelinev1alpha1.InputParam{
-					{Name: "testparam", Value: "testvalue"},
-					{Name: "testparam2", Value: "testvalue2"},
-				},
-			},
-		},
-	}
-	k8sClient.Create(ctx, pipelineTrigger)
-	return pipelineTrigger
-}
